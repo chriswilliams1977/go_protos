7feac2953f34c07b5d515a87e928526c0db682ac
chriswilliams1977 <chriswilliams1977@gmail.com>
2021-01-22 09:38:47 +0100

    snippet headers, qs and project structure

--- snippets/cmd/web/handlers.go
+++ snippets/cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"log"
 	"strconv"
 )
 
@@ -52,11 +51,11 @@ func createSnippet(w http.ResponseWriter, r *http.Request){
 	if r.Method != "POST" {
 		w.Header().Set("Allow","POST")
 		/*
-		//this is an example - in reality you dont call directly you use http.Error
-		//you must call w.WriteHeader with response code before w.Write or a 200 will be passed
-		//to test use curl -i -X POST http://localhost:4000/snippet/create
-		w.WriteHeader(405)
-		w.Write([]byte("Method not allowed"))
+			//this is an example - in reality you dont call directly you use http.Error
+			//you must call w.WriteHeader with response code before w.Write or a 200 will be passed
+			//to test use curl -i -X POST http://localhost:4000/snippet/create
+			w.WriteHeader(405)
+			w.Write([]byte("Method not allowed"))
 		*/
 		//common way to handle response code and pass message to header
 		http.Error(w,"Method not allowed",405)
@@ -64,38 +63,4 @@ func createSnippet(w http.ResponseWriter, r *http.Request){
 	}
 
 	w.Write([]byte("Create new snippet"))
-}
-
-func main(){
-
-	//Mux is a request handler -  It matches the URL of each incoming request against a list of registered patterns
-	//and calls the handler for the pattern that most closely matches the URL.
-
-	//servemux supports two URL patterns
-	//fixed - dont end with / - /snippet
-	//subtree - end with / - like root - catches everything underneath it
-	//HandleFunc - register routes with DefaultServeMux (regular servermux) behind the scenes - var DefaultServeMux = NewServeMux()
-	//host patterns processed first - foo.example.org/ before /baz
-	//longer url patterns get priority over short url patterns
-
-	//DefaultServeMux is a global var so other 3rd party packages can access it  if compromised = security risk
-	//thus better to create own locally-scoped servemux instead
-	mux := http.NewServeMux()
-	//route requests to root to home handler
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit.
-	log.Println("Starting server on :4000")
-	//address should be in format :port
-	//only specific host if machine has multi network interfaces
-	//if you use :name (:http) go will look up port number from /ect/services
-	err := http.ListenAndServe(":4000", mux)
-
-	//log.fatal calls os.Exit(1) after writing message thus causing app to exit immediately
-	log.Fatal(err)
 }
---
+++ snippets/cmd/web/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+func main(){
+
+	//Mux is a request handler -  It matches the URL of each incoming request against a list of registered patterns
+	//and calls the handler for the pattern that most closely matches the URL.
+
+	//servemux supports two URL patterns
+	//fixed - dont end with / - /snippet
+	//subtree - end with / - like root - catches everything underneath it
+	//HandleFunc - register routes with DefaultServeMux (regular servermux) behind the scenes - var DefaultServeMux = NewServeMux()
+	//host patterns processed first - foo.example.org/ before /baz
+	//longer url patterns get priority over short url patterns
+
+	//DefaultServeMux is a global var so other 3rd party packages can access it  if compromised = security risk
+	//thus better to create own locally-scoped servemux instead
+	mux := http.NewServeMux()
+	//route requests to root to home handler
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass in
+	// two parameters: the TCP network address to listen on (in this case ":4000")
+	// and the servemux we just created. If http.ListenAndServe() returns an error
+	// we use the log.Fatal() function to log the error message and exit.
+	log.Println("Starting server on :4000")
+	//address should be in format :port
+	//only specific host if machine has multi network interfaces
+	//if you use :name (:http) go will look up port number from /ect/services
+	err := http.ListenAndServe(":4000", mux)
+
+	//log.fatal calls os.Exit(1) after writing message thus causing app to exit immediately
+	log.Fatal(err)
+}
