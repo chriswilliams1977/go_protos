f792dbbbe0b6d7ec472b8a6997e7138ef6e128ca
chriswilliams1977 <chriswilliams1977@gmail.com>
2021-03-23 11:33:16 +0100

    testing TF with CB

--- snippets/cmd/main.go
+++ snippets/cmd/main.go
@@ -36,11 +36,13 @@ func main(){
 	// additional information to include (local date and time). Note that the flags
 	// are joined using the bitwise OR operator |.
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
 	// Create a logger for writing error messages in the same way, but use stderr as
 	// the destination and use the log.Lshortfile flag to include the relevant
 	// file name and line number.
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	//Get static files path and set to defaul if no value is passed
 	uiPath := snippets.GetEnv("UI_PATH","./web/")
 
 	// Initialize a new instance of application containing the dependencies.
