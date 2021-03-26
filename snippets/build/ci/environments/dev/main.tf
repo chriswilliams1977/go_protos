d541240f8f3d2cb6f75c94082a6e03a1f38bad36
chriswilliams1977 <chriswilliams1977@gmail.com>
2021-03-26 15:03:30 +0100

    updating tf v12 syntax

--- snippets/build/ci/environments/dev/main.tf
+++ snippets/build/ci/environments/dev/main.tf
@@ -1,19 +1,19 @@
 locals {
-  "env" = "dev"
+  env = "dev"
 }
 
 provider "google" {
-  project = "${var.project}"
+  project = var.project
 }
 
 module "vpc" {
   source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
+  project = var.project
+  env     = local.env
 }
 
 module "firewall" {
   source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
+  project = var.project
+  subnet  = module.vpc.subnet
 }
